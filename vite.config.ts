ad4e342ed83a383d10c4d6987eeb101c8e7eabd5
Ha Quanq <quangha0120010@gmail.com>
2025-10-22 16:09:11 +0700

    Config path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 import { defineConfig } from "vite";
 import svgLoader from "vite-svg-loader";
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [vue(), svgLoader({ defaultImport: "url" }), tailwindcss()],
+    resolve: {
+        alias: { "@": path.resolve(__dirname, "./src") },
+    },
     base: "/weather-app",
 });
